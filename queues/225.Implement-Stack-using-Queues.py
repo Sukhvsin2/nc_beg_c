a0902845a7e5fe1ec2bfd4d9f5fcd235f1862cdb
Sukhvinder Singh <46042060+Sukhvsin2@users.noreply.github.com>
2024-02-22 11:21:32 -0500

    Update 225.Implement-Stack-using-Queues.py
    added c++ code in comments

--- queues/225.Implement-Stack-using-Queues.py
+++ queues/225.Implement-Stack-using-Queues.py
@@ -36,3 +36,46 @@ class MyStack(object):
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+# C++ code here
+'''
+class MyStack {
+    queue<int> q;
+
+public:
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q.push(x);
+    }
+    
+    int pop() {
+        for(int i=0;i<q.size()-1;i++){
+            int temp = q.front();
+            q.pop();
+            q.push(temp);
+        }
+        int f = q.front();
+        q.pop();
+        return f;
+    }
+    
+    int top() {
+        return q.back();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+'''
